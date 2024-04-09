3034c2a0f84cb79bdec2e0c66b7ae7f235c9685d
Bono <kubonkwan99@gmail.com>
2024-04-09 11:48:16 +0800

    chore: extend cache expiration

--- src/redis.ts
+++ src/redis.ts
@@ -1,6 +1,6 @@
 import { createClient } from "redis";
 
-export const expirationTime = 60 * 30; // NOTE: used 30 mins to limit my cache usage (using free plan)
+export const expirationTime = 60 * 60 * 24; // NOTE: used 1 day to limit my cache usage (using free plan)
 
 const redisClient = createClient({
   url: process.env.REDIS_URL,
